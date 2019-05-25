31e94fd8ba50480ffc50cfeaf8b1c4e4cfc1e92b
heyunfeng <heyunfeng@baidu.com>
2019-05-26 00:06:05 +0800

    压缩

--- src/pages/workBench/workBench.js
+++ src/pages/workBench/workBench.js
@@ -2,14 +2,8 @@ import Vue from 'vue'
 import App from './workBench.vue'
 import iView from 'iview';
 import 'iview/dist/styles/iview.css';
-import VueHighcharts from 'vue-highcharts';
-import Highcharts from 'highcharts';
-
-import loadMap from 'highcharts/modules/map.js';
-loadMap(Highcharts);
 
 Vue.use(iView);
-Vue.use(VueHighcharts, { Highcharts });
 
 Vue.config.productionTip = false
 
